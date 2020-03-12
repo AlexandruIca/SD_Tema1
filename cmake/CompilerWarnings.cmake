0fc337e17953166b40d284afe94c7a127df68b0d
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-03-12 13:33:02 +0200

    Apply cmake-format

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,44 +1,39 @@
 function(set_project_warnings project_name)
-    option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors." TRUE)
+  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors." TRUE)
 
-    set(MSVC_WARNINGS
-        /W4 # Reasonable, /Wall is madness
-        /analyze
-        /permissive-)
+  set(MSVC_WARNINGS /W4 # Reasonable, /Wall is madness
+                    /analyze /permissive-)
 
-    set(CLANG_WARNINGS
-        -Wall
-        -Wextra
-        -Wshadow
-        -Wnon-virtual-dtor
-        -Wold-style-cast
-        -Wcast-align
-        -Wunused
-        -Woverloaded-virtual
-        -Wpedantic
-        -Wconversion
-        -Wsign-conversion
-        -Wnull-dereference)
+  set(CLANG_WARNINGS
+      -Wall
+      -Wextra
+      -Wshadow
+      -Wnon-virtual-dtor
+      -Wold-style-cast
+      -Wcast-align
+      -Wunused
+      -Woverloaded-virtual
+      -Wpedantic
+      -Wno-conversion
+      -Wno-sign-conversion
+      -Wno-sign-compare
+      -Wnull-dereference)
 
-    if(WARNINGS_AS_ERRORS)
-        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-        set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
-    endif()
+  if(WARNINGS_AS_ERRORS)
+    set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+    set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+  endif()
 
-    set(GCC_WARNINGS
-        ${CLANG_WARNINGS}
-        -Wmisleading-indentation
-        -Wduplicated-cond
-        -Wuseless-cast)
+  set(GCC_WARNINGS ${CLANG_WARNINGS} -Wmisleading-indentation -Wduplicated-cond
+                   -Wuseless-cast)
 
+  if(MSVC)
+    set(PROJECT_WARNINGS ${MSVC_WARNINGS})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(PROJECT_WARNINGS ${CLANG_WARNINGS})
+  else()
+    set(PROJECT_WARNINGS ${GCC_WARNINGS})
+  endif()
 
-    if(MSVC)
-        set(PROJECT_WARNINGS ${MSVC_WARNINGS})
-    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        set(PROJECT_WARNINGS ${CLANG_WARNINGS})
-    else()
-        set(PROJECT_WARNINGS ${GCC_WARNINGS})
-    endif()
-
-    target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
+  target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
 endfunction()
